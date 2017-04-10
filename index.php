31eb7aef31a2f5afce0a50927edf0b63f5feeab7
кирилл горюнов < >
2017-04-10 19:22:50 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,298 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>
+    Главная страница - Master Keys
+  </title>
+  <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+</head>
+<body>
+<div class="layout">
+  <header class="top">
+    <div class="head-layout">
+      <nav class="main-menu">
+        <ul class="menu">
+          <li><a href="#">Гарантии</a></li>
+          <li><a href="#">Отзывы</a></li>
+          <li><a href="#">Помощь</a></li>
+          <li><a href="#">Контакты</a></li>
+        </ul>
+      </nav>
+      <div class="after-menu">
+        <div class="logo">
+          <a href = "#"></a>
+        </div>
+        <div class="login-but">
+          <a href="#">Регистрация</a>
+          <a href="#">Вход</a>
+        </div>
+      </div>
+    </div>
+    <div id="live-lenta-block">
+      <div class="live-lenta">
+        <div class="lenta-block info">
+          <div class='lenta-info-head'>
+            LIVE-лента
+          </div>
+          <div class='lenta-info-text'>
+            В ленте отображаются игры <br>полученные реальными клиентами!
+          </div>
+        </div>
+        <div class="lenta-block lenta-game" data-id="">
+          <div class="lenta-image"></div>
+          <div class="lenta-name">Mr.sokolov.dave</div>
+          <div class="lenta-ago">5 с. назад</div>
+        </div>
+        <div class="lenta-block lenta-game" data-id="">
+          <div class="lenta-image"></div>
+          <div class="lenta-name">Drasadaweadw</div>
+          <div class="lenta-ago">5 с. назад</div>
+        </div>
+      </div>
+      <div class="clear"></div>
+    </div>
+  </header>
+  <div class="clear"></div>
+  <div class="main">
+    <div class="rand-block">
+      <div class="rand-set">
+        <div class="rand-head">
+          Ваш приз
+          <div class="rand-line"></div>
+        </div>
+        <div class="rand-block-games">
+          <div class="rand-games">
+              <div class="rand-game">
+              </div>
+              <div class="rand-game">
+              </div>
+              <div class="rand-game">
+              </div>
+              <div class="rand-game">
+              </div>
+              <div class="rand-game">
+              </div>
+              <div class="clear"></div>
+          </div>
+        </div>
+      </div>
+      <div class="rand-button-block">
+        <div class="rand-button">
+          <a href="">Крутить рулетку за 59 рублей</a>
+        </div>
+        <div class="rand-sod">
+          <a href="">Содержимое</a>
+        </div>
+      </div>
+
+
+      <div class="block-games">
+        <div class="head-games">
+          <div class="left-head-games"></div>
+          <div class="center-head-games">Игры</div>
+          <div class="right-head-games"></div>
+        </div>
+        <div class="main-games">
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+              <div class="name">Left4Dead</div>
+              <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+              <div class="name">Left4Dead</div>
+              <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="clear"></div>
+        </div>
+      </div>
+
+      <div class="block-games">
+        <div class="head-games">
+          <div class="left-head-games"></div>
+          <div class="center-head-games">Кейсы</div>
+          <div class="right-head-games"></div>
+        </div>
+        <div class="main-games">
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+              <div class="name">Left4Dead</div>
+              <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+              <div class="name">Left4Dead</div>
+              <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="game">
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="clear"></div>
+        </div>
+      </div>
+
+      <div class="block-games">
+        <div class="head-games">
+          <div class="left-head-games"></div>
+          <div class="center-head-games">Все или ничего</div>
+          <div class="right-head-games"></div>
+        </div>
+        <div class="main-games">
+          <div class="block-nothing">
+            <div class="left-game"></div>
+            <div class="right-game"></div>
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="block-nothing">
+            <div class="left-game"></div>
+            <div class="right-game"></div>
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="block-nothing">
+            <div class="left-game"></div>
+            <div class="right-game"></div>
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="block-nothing">
+            <div class="left-game"></div>
+            <div class="right-game"></div>
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="block-nothing">
+            <div class="left-game"></div>
+            <div class="right-game"></div>
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="block-nothing">
+            <div class="left-game"></div>
+            <div class="right-game"></div>
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="block-nothing">
+            <div class="left-game"></div>
+            <div class="right-game"></div>
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="block-nothing">
+            <div class="left-game"></div>
+            <div class="right-game"></div>
+            <div class="name">Left4Dead</div>
+            <div class="game-price">500 &#8381;</div>
+          </div>
+          <div class="clear"></div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <footer>
+    <div class="wrapper">
+      <div class="col">
+        <div class="payments">
+        </div>
+          <span>MONSTERKEYS<br>
+          2017 ©
+          </span>
+      </div>
+      <div class="col">
+        <b>О нас</b>
+        <ul class="footer-menu">
+          <li><a href="#">Гарантии</a></li>
+          <li><a href="#">Отзывы</a></li>
+          <li><a href="#">Помощь</a></li>
+          <li><a href="#">Контакты</a></li>
+        </ul>
+      </div>
+
+      <div class="clear"></div>
+      <div class="footer-text">
+          <h1>Что такое испытай удачу crazykeys.ru?</h1>
+          <div class="footer-text-cont">
+            Это <b>сервис для геймеров</b>, созданный фанатами компьютерных игр. Более того, мы предлагаем сыграть в игру, не покидая страницу сайта.
+            Для этого нужно совсем немного: символическая сумма в 69 рублей и немного везения.
+            Вам открывается уникальная возможность <b>купить стим ключ по низкой цене</b> и приобщиться к сообществу знаменитых игровых хитов.
+          </div>
+          <h2>Выгода от покупки рандомного steam ключа</h2>
+          <div class="footer-text-cont">
+            Наш сайт нельзя сравнивать с рулеткой или казино: эти организации закладывают процент проигрыша на уровне 85%, а мы - <b>гарантируем 100% получение приза</b>.
+            Каким он будет - зависит от вашей удачи и легкой руки. Вполне возможно, это будет хитовая игра GTA 5 или знаменитая стратегия Цивилизация 6.
+            Вы вкладываете всего лишь 1 доллар и получаете шанс выиграть игру ценой <b>до 3290 рублей</b>. Как видите, выгода очевидна.
+          </div>
+          <h2>Какие игры можно получить</h2>
+          <div class="footer-text-cont">
+            Вы можете <b>купить рандом стим ключ</b> и порадовать себя новеньким шутером, классической ролевой игрой или недавно вышедшим спортивным симулятором.
+            Купить steam ключ на нашем сайте можно любым удобным для Вас способом: большой выбор способов оплаты делает сервис еще более удобным и привлекательным.
+            <b>Испытай удачу стим</b>: тебе сегодня обязательно повезет!
+          </div>
+      </div>
+    </div>
+  </footer>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+
+  if (isset($_POST['email']) && $_POST['email'])
+    $email = $_POST['email'];
+  else {
+    die('Введите email');
+  }
+  if (isset($_POST['password']) && $_POST['password'])
+    $password = $_POST['password'];
+  else {
+      die('Введите пароль!');
+  }
+  // подключение к БД
+  $mysqli = new mysqli("127.0.0.1", "root", "", "monsterkeys");
+  if ($mysqli->connect_errno) {
+    die("Failed to connect to MySQL: " . $mysqli->connect_error);
+  }
+
+
+  // $res = $mysqli->query("SELECT id FROM user WHERE email='$email'");
+  // $password = md5('QWERTY'.$password.'QWERTY');
+  // if(!$row = $res->fetch_assoc()){
+  //   die('Пользователь уже зарегистрирован!');
+  // };
+  //
+  //
+  // if (!$mysqli->query("INSERT INTO user(email, password) VALUES('$email','$password')")) {
+  //   die("Не удалось выполнить вставку: (" . $mysqli->errno . ") " . $mysqli->error);
+  // }
+  //
+  // echo "Пользователь $email успешно зарегистрирован";
+  //
